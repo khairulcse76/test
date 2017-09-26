f2470dce82feb1ee7bb9bffb8cac7fee98583cf0
Md. Khairul Islam <khairulislamtpi76@gmail.com>
2017-09-26 18:52:22 +0600

    authentication success

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,16 @@ Route::get('/home', 'HomeController@index');
 
 Route::group([
     'prefix' => Config("authorization.route-prefix"),
+    'middleware' =>  ['authorize', 'auth']
+],
+    function() {
+        Route::get('/sayhello', 'sayHelloController@index');
+    });
+
+
+
+Route::group([
+    'prefix' => Config("authorization.route-prefix"),
     'middleware' => ['web', 'auth']],
     function() {
         Route::group(['middleware' => Config("authorization.middleware")], function() {
@@ -22,7 +32,6 @@ Route::group([
             Route::post('/permissions', 'PermissionsController@update');
             Route::post('/permissions/getSelectedRoutes', 'PermissionsController@getSelectedRoutes');
         });
-
         Route::get('/', function () {
             return view('vendor.authorize.welcome');
         });
